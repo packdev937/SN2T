0bd7fd2acff92807f1e28f1d4044c7cc1b2d5e22
gundorit <packdev937@gmail.com>
2023-12-27 17:35:48 +0900

    initial commit

---
+++ src/main/java/com/example/sn2t/Sn2TApplication.java
@@ -0,0 +1,13 @@
+package com.example.sn2t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sn2TApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sn2TApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sn2t/Sn2TApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sn2t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sn2TApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
