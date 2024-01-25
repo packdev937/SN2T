8995270646e32b11bf8674531488321e5fe14d0c
Geonwoo Pack <packdev937@gmail.com>
2024-01-26 03:57:25 +0900

    #3 feat : Status Properties의 상태 변경을 추가한다
    후에 재사용성을 위해 리팩토링을 진행한다

---
+++ src/main/java/com/example/sn2t/notion/application/PageService.java
@@ -0,0 +1,47 @@
+package com.example.sn2t.notion.application;
+
+
+import static com.example.sn2t.notion.domain.notion.NotionProperties.*;
+
+import com.example.sn2t.notion.domain.notion.NotionProperties;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class PageService {
+
+    private final WebClient webClient;
+
+    public PageService() {
+        this.webClient = WebClient.builder()
+            .baseUrl(pageBaseUrl.get())
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    }
+
+    /*
+     * to는 바꾸고자 하는 상태로, "UNCOMPLETED", "UPLOADING" 또는 "COMPLETED"가 될 수 있습니다.
+     */
+    public void updateStatusProperties(String pageId, String secretKey, String to) {
+        String jsonBody = "{\n"
+            + "  \"properties\": {\n"
+            + "    \"status\": {\n"
+            + "      \"select\": {\n"
+            + "        \"name\": \"" + to + "\"\n"
+            + "      }\n"
+            + "    }\n"
+            + "  }\n"
+            + "}\n";
+
+        webClient.patch()
+            .uri(pageId)
+            .header("Authorization", "Bearer " + secretKey)
+            .header("Notion-Version", "2022-06-28")
+            .bodyValue(jsonBody)
+            .retrieve()
+            .toBodilessEntity()
+            .block();
+    }
+}
--- src/main/java/com/example/sn2t/notion/domain/notion/NotionProperties.java
+++ src/main/java/com/example/sn2t/notion/domain/notion/NotionProperties.java
@@ -2,7 +2,8 @@ package com.example.sn2t.notion.domain.notion;
 
 public enum NotionProperties {
     databaseBaseUrl("https://api.notion.com/v1/databases/"),
-    blockBaseUrl("https://api.notion.com/v1/blocks/");
+    blockBaseUrl("https://api.notion.com/v1/blocks/"),
+    pageBaseUrl("https://api.notion.com/v1/pages/");
 
     NotionProperties(String description) {
         this.description = description;
